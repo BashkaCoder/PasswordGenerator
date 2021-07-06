533431b70e0a3c3131779462dab9d21e6eedcc61
Данила Башинский <dbashinskiy@miem.hse.ru>
2021-07-06 16:30:52 +0300

    Version 0.1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+void showMenu();
+
 int main()
 {
 	std::cout << "Testing purposes" << std::endl;
 
 	return 0;
+}
+
+void showMenu()
+{
+	std::cout << "Choose option from list below: \n"
+		<< "1 - Generate 1 password.\n"
+		<< "2 - Generate several passwords. \n"
+		<< "3 - Generate 1 password to file. \n"
+		<< "4 - Generate several passwords to file. \n";
 }
